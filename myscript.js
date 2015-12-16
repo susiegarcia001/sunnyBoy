5ce991da08b875549f00d7391ef55193144fa49d
susieq0101 <susieq0101@gmail.com>
2015-12-16 20:58:21 +0000

    first commit

---
+++ myscript.js
@@ -0,0 +1,275 @@
+$(document).ready(function(){
+   // start of bubble
+   $(".puppyone").hover(function(){
+      $(".talkone").show(1500);
+    });
+    $(".puppyone").hover(function(){
+      $(".talkone").hide(1500);
+    });
+    $(".puppyone").hover(function() {
+      $(".talkone").stop()
+   });
+   
+   $(".puppytwo").hover(function(){
+      $(".talktwo").show(1500);
+    });
+    $(".puppytwo").hover(function(){
+      $(".talktwo").hide(1500);
+    });
+    $(".puppytwo").hover(function() {
+      $(".talktwo").stop()
+   });
+   
+   $(".puppythree").hover(function(){
+      $(".talkthree").show(1500);
+    });
+    $(".puppythree").hover(function(){
+      $(".talkthree").hide(1500);
+    });
+    $(".puppythree").hover(function() {
+      $(".talkthree").stop()
+   });
+   
+   $(".puppyfour").hover(function(){
+      $(".talkfour").show(1500);
+    });
+    $(".puppyfour").hover(function(){
+      $(".talkfour").hide(1500);
+    });
+    $(".puppyfour").hover(function() {
+      $(".talkfour").stop()
+   });
+   
+   $(".puppyfive").hover(function(){
+      $(".talkfive").show(1500);
+    });
+    $(".puppyfive").hover(function(){
+      $(".talkfive").hide(1500);
+    });
+    $(".puppyfive").hover(function() {
+      $(".talkfive").stop()
+   });
+   
+   $(".puppysix").hover(function(){
+      $(".talksix").show(1500);
+    });
+    $(".puppysix").hover(function(){
+      $(".talksix").hide(1500);
+    });
+    $(".puppysix").hover(function() {
+      $(".talksix").stop()
+   });
+   
+   $(".puppyseven").hover(function(){
+      $(".talkseven").show(1500);
+    });
+    $(".puppyseven").hover(function(){
+      $(".talkseven").hide(1500);
+    });
+    $(".puppyseven").hover(function() {
+      $(".talkseven").stop()
+   });
+   
+   $(".puppyeight").hover(function(){
+      $(".talkeight").show(1500);
+    });
+    $(".puppyeight").hover(function(){
+      $(".talkeight").hide(1500);
+    });
+    $(".puppyeight").hover(function() {
+      $(".talkeight").stop()
+   });
+   
+   $(".puppynine").hover(function(){
+      $(".talknine").show(1500);
+    });
+    $(".puppynine").hover(function(){
+      $(".talknine").hide(1500);
+    });
+    $(".puppynine").hover(function() {
+      $(".talknine").stop()
+   });
+   
+   $(".puppyten").hover(function(){
+      $(".talkten").show(1500);
+    });
+    $(".puppyten").hover(function(){
+      $(".talkten").hide(1500);
+    });
+    $(".puppyten").hover(function() {
+      $(".talkten").stop()
+   });
+   
+   $(".puppyeleven").hover(function(){
+      $(".talkeleven").show(1500);
+    });
+    $(".puppyeleven").hover(function(){
+      $(".talkeleven").hide(1500);
+    });
+    $(".puppyeleven").hover(function() {
+      $(".talkeleven").stop()
+   });
+   
+   $(".puppytwelve").hover(function(){
+      $(".talktwelve").show(1500);
+    });
+    $(".puppytwelve").hover(function(){
+      $(".talktwelve").hide(1500);
+    });
+    $(".puppytwelve").hover(function() {
+      $(".talktwelve").stop()
+   });
+   
+   $(".puppythirteen").hover(function(){
+      $(".talkthirteen").show(1500);
+    });
+    $(".puppythirteen").hover(function(){
+      $(".talkthirteen").hide(1500);
+    });
+    $(".puppythirteen").hover(function() {
+      $(".talkthirteen").stop()
+   });
+   
+   $(".puppyfourteen").hover(function(){
+      $(".talkfourteen").show(1500);
+    });
+    $(".puppyfourteen").hover(function(){
+      $(".talkfourteen").hide(1500);
+    });
+    $(".puppyfourteen").hover(function() {
+      $(".talkfourteen").stop()
+   });
+   // end of bubble
+   
+  
+
+  
+   // start of text
+   $(".puppyone").hover(function(){
+      $(".parone").fadeOut(2000);
+      }, function(){
+      $(".parone").fadeIn(2000);
+      }, function(){
+      $(".parone").stop();   
+      
+   });
+   
+   $(".puppytwo").hover(function(){
+      $(".partwo").fadeOut(2000);
+      }, function(){
+      $(".partwo").fadeIn(2000);
+      }, function(){
+      $(".partwo").stop();   
+      
+   });
+   
+   $(".puppythree").hover(function(){
+      $(".parthree").fadeOut(2000);
+      }, function(){
+      $(".parthree").fadeIn(2000);
+      }, function(){
+      $(".parthree").stop();   
+      
+   });
+   
+   $(".puppyfour").hover(function(){
+      $(".parfour").fadeOut(2000);
+      }, function(){
+      $(".parfour").fadeIn(2000);
+      }, function(){
+      $(".parfour").stop();   
+      
+   });
+   
+   $(".puppyfive").hover(function(){
+      $(".parfive").fadeOut(2000);
+      }, function(){
+      $(".parfive").fadeIn(2000);
+      }, function(){
+      $(".parfive").stop();   
+      
+   });
+   
+   $(".puppysix").hover(function(){
+      $(".parsix").fadeOut(2000);
+      }, function(){
+      $(".parsix").fadeIn(2000);
+      }, function(){
+      $(".parsix").stop();   
+      
+   });
+   
+   $(".puppyseven").hover(function(){
+      $(".parseven").fadeOut(2000);
+      }, function(){
+      $(".parseven").fadeIn(2000);
+      }, function(){
+      $(".parseven").stop();   
+      
+   });
+   
+   $(".puppyeight").hover(function(){
+      $(".pareight").fadeOut(2000);
+      }, function(){
+      $(".pareight").fadeIn(2000);
+      }, function(){
+      $(".pareight").stop();   
+      
+   });
+   
+   $(".puppynine").hover(function(){
+      $(".parnine").fadeOut(2500);
+      }, function(){
+      $(".parnine").fadeIn(2500);
+      }, function(){
+      $(".parnine").stop();   
+      
+   });
+   
+   $(".puppyten").hover(function(){
+      $(".parten").fadeOut(2000);
+      }, function(){
+      $(".parten").fadeIn(2000);
+      }, function(){
+      $(".parten").stop();   
+      
+   });
+   
+   $(".puppyeleven").hover(function(){
+      $(".pareleven").fadeOut(2000);
+      }, function(){
+      $(".pareleven").fadeIn(2000);
+      }, function(){
+      $(".pareleven").stop();   
+      
+   });
+   
+   $(".puppytwelve").hover(function(){
+      $(".partwelve").fadeOut(2000);
+      }, function(){
+      $(".partwelve").fadeIn(2000);
+      }, function(){
+      $(".partwelve").stop();   
+      
+   });
+   
+   $(".puppythirteen").hover(function(){
+      $(".parthirteen").fadeOut(2000);
+      }, function(){
+      $(".parthirteen").fadeIn(2000);
+      }, function(){
+      $(".parthirteen").stop();   
+      
+   });
+   
+   $(".puppyfourteen").hover(function(){
+      $(".parfourteen").fadeOut(2000);
+      }, function(){
+      $(".parfourteen").fadeIn(2000);
+      }, function(){
+      $(".parfourteen").stop();   
+      
+   });
+   // end of text
+
+});
